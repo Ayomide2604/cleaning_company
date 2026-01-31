2fc9706ecb6e3a33f55dd596978c120638d65b80
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-01-31 16:06:06 -0700

    added template assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BootstrapClient from "@/components/BootstrapClient";
+
+/* Template CSS */
+import "@/assets/css/tidytouch.css";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
